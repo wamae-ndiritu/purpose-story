7d68cd5de19c3744cd3c5c9b3f6ffb6c85edb4f6
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-10-07 19:32:06 +0300

    Implement sharing response using emailjs

--- frontend/src/components/ShareModal.jsx
+++ frontend/src/components/ShareModal.jsx
@@ -1,6 +1,66 @@
 import React from "react";
+import emailjs from "@emailjs/browser";
+import { useSelector } from "react-redux";
+
+const serviceID = process.env.REACT_APP_SERVICE_ID;
+const templateID = process.env.REACT_APP_TEMPLATE_ID;
+const publicKey = process.env.REACT_APP_PUBLIC_KEY;
 
 const ShareModal = ({ closeModal }) => {
+  const purposeStory = useSelector((state) => state.purposeStory);
+  const { item } = purposeStory;
+
+  const questions = {
+    question_1: "Purpose Statement",
+    question_2: "Origin and Personal Connection",
+    question_3: "Vision and Mission",
+    question_4: "Values and Beliefs",
+    question_5: "Impact and Beneficiaries",
+    question_6: "Actions and Commitments",
+  };
+
+  const answers = {
+    answer_1: item?.purpose,
+    answer_2: item?.origin,
+    answer_3:
+      item?.vissionAndMission?.vision +
+      item?.vissionAndMission?.mission +
+      item?.vissionAndMission?.impact,
+    answer_4: item?.valuesAndBeliefs?.values + item?.valuesAndBeliefs?.beliefs,
+    answer_5:
+      item?.impactAndBeneficiaries?.impact +
+      item.impactAndBeneficiaries?.beneficiaries,
+    answer_6:
+      item?.actionsAndCommitments?.actions +
+      item?.actionsAndCommitments?.examples +
+      item?.actionsAndCommitments?.dedication,
+  };
+
+  const templateParams = {
+    user_name: "Ndiritu Wa Wamai",
+    user_email: "nelitetech@gmail.com",
+    ...answers,
+    ...questions,
+  };
+
+  console.log(templateParams);
+  const shareResponses = () => {
+    emailjs
+      .send(
+        "service_iz21rs6",
+        "template_niqb61f",
+        templateParams,
+        "KehPQQgwKRR6ja30g"
+      )
+      .then((response) => {
+        console.log(response.text);
+        closeModal();
+      })
+      .catch((error) => {
+        console.log("FAILED...", error);
+        closeModal();
+      });
+  };
   return (
     <div class='fixed inset-0 flex items-center justify-center z-50'>
       <div class='fixed inset-0 bg-black opacity-50'></div>
@@ -34,7 +94,10 @@ const ShareModal = ({ closeModal }) => {
             more meaningful purpose.
           </p>
           <div className='w-full flex justify-between'>
-            <button class='bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded'>
+            <button
+              class='bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded'
+              onClick={shareResponses}
+            >
               OK
             </button>
             <button
