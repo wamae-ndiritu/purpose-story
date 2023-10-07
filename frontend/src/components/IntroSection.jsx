6754278a3f3c58e54be39d14085bd3763b3193ff
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-10-07 19:44:23 +0300

    Add intro section

---
+++ frontend/src/components/IntroSection.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const IntroSection = () => {
+  return (
+    <section className='bg-gray-200 py-16 text-gray-600 mt-5'>
+      <div className='container mx-auto text-center px-4'>
+        <h1 className='text-4xl font-bold mb-4'>
+          Unlock Your Purpose with My Purpose Story
+        </h1>
+        <p className='text-lg mb-8'>
+          My Purpose Story (MPS) is a powerful narrative tool that helps
+          individuals and organizations articulate their sense of purpose. It
+          goes beyond the 'why' and dives deep into the 'why you,' allowing you
+          to express the underlying motivations, values, and goals that drive
+          your actions and decisions.
+        </p>
+        <p className='text-lg mb-8'>
+          If you haven’t fully developed your purpose and purpose statement, My
+          Purpose Story can guide you through the Purpose Clarification Tool,
+          setting you on the path to a more meaningful and purposeful existence.
+        </p>
+        <Link
+          to='/questions/purpose'
+          className='bg-white text-gray-800 hover:bg-gray-800 hover:text-white py-2 px-6 rounded-full text-lg font-semibold inline-block transition duration-300'
+        >
+          Get Started
+        </Link>
+      </div>
+    </section>
+  );
+};
+
+export default IntroSection;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,11 +3,13 @@ import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import HowItWorksSection from "../components/HowItWorks";
 import Footer from "../components/Footer";
+import IntroSection from "../components/IntroSection";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <IntroSection />
       <Hero />
       <HowItWorksSection />
       <Footer />
